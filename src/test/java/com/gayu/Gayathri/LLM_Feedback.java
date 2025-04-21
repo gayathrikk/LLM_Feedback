b03102f79364181eb17fccf86e69a3d0c9279154
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-04-21 10:18:40 +0530

    Update LLM_Feedback.java

--- src/test/java/com/gayu/Gayathri/LLM_Feedback.java
+++ src/test/java/com/gayu/Gayathri/LLM_Feedback.java
@@ -55,7 +55,7 @@ public class LLM_Feedback {
 		WebDriverWait wait2 = new WebDriverWait(driver, 20);
 		WebElement emailInput = wait2
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='email']")));
-		emailInput.sendKeys("softwaretestingteam9@gmail.com");
+		emailInput.sendKeys("teamsoftware457@gmail.com");
 		System.out.println("--------------------------*****************-----------------------");
 		System.out.println("Mail I'd is entered");
 		WebDriverWait wait3 = new WebDriverWait(driver, 20);
